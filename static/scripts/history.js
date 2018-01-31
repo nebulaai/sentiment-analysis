840bdf97d16002a03274f8983af2505aa9381b45
Nebula-Chi <zcjswx@gmail.com>
2018-01-31 17:02:28 -0500

    display output

--- static/scripts/history.js
+++ static/scripts/history.js
@@ -1,5 +1,6 @@
 
 function TaskInfo(contract){
+    this.index = 0;
     this.contract = contract;
     this.error = false;
     this.address ="";
@@ -26,7 +27,8 @@ let complete_task_history_address = [];
  * @param from
  * @param to
  */
-const get_task_history = function (from, to ) {
+/*const get_task_history =*/
+( function () {
     initiateContract(function () {                                                              
         loadContract(
             function () {
@@ -37,71 +39,75 @@ const get_task_history = function (from, to ) {
 
                         complete_task_history_address = result;
 
-                        let show_num = from;
+                        let length = result.length;
 
-                        if(from === undefined) {
-                            if (to === undefined) to = 15;
-                            show_num = result.length - 1 >= to ? to : result.length - 1;
-                        }
+                        // if(from === undefined) {
+                        //     if (to === undefined) to = 15;
+                        //     show_num = length - 1 >= to ? to : length - 1;
+                        // }
 
                         //only show 15 if more than 15
                         //the rest will be loaded if needed
-
-                        for (let i = show_num; i >= 0; i--) {
-
-                            let index = result.length - i -1;
+                        let index = 0;
+                        let end = length > 15 ? length - 15 : 0;
+                        for (let i = length - 1; i >= end; i--) {
 
                             let taskInfo = new TaskInfo(window.taskContract.at(result[i]));
+                            taskInfo.index = index;
                             my_task_list.push(taskInfo);
 
                             taskInfo.contract.task_id(function (error, result) {
                                 if (error) console.log(error);
-                                else my_task_list[index].task_id = Number(result);
+                                else {
+                                    my_task_list[taskInfo.index].task_id = Number(result);
+                                }
                             });
                             taskInfo.contract.uuid(function (error, result) {
                                 if (error) console.log(error);
-                                else my_task_list[index].uuid = web3.toAscii(result);
+                                else my_task_list[taskInfo.index].uuid = web3.toAscii(result);
                             });
                             taskInfo.contract.started(function (error, result) {
                                 if (error) console.log(error);
-                                else my_task_list[index].started = result;
+                                else my_task_list[taskInfo.index].started = result;
                             });
                             taskInfo.contract.task_issue(function (error, result) {
                                 if (error) console.log(error);
-                                else my_task_list[index].has_issue = result;
+                                else my_task_list[taskInfo.index].has_issue = result;
                             });
                             taskInfo.contract.completed(function (error, result) {
                                 if (error) console.log(error);
-                                else my_task_list[index].completed = result;
+                                else my_task_list[taskInfo.index].completed = result;
                             });
                             taskInfo.contract.worker(function (error, result) {
                                 if (error) console.log(error);
-                                else my_task_list[index].worker = result;
+                                else my_task_list[taskInfo.index].worker = result;
                             });
                             taskInfo.contract.task_name(function (error, result) {
                                 if(error) console.log(error);
-                                else my_task_list[index].name = web3.toAscii(result);
+                                else my_task_list[taskInfo.index].name = web3.toAscii(result);
                             });
                             taskInfo.contract.data_address(function (error, result) {
                                 if(error) console.log(error);
-                                else my_task_list[index].data_address = result;
+                                else my_task_list[taskInfo.index].data_address = result;
                             });
                             taskInfo.contract.script_address(function (error, result) {
                                 if(error) console.log(error);
-                                else my_task_list[index].script_address = result;
+                                else my_task_list[taskInfo.index].script_address = result;
                             });
                             taskInfo.contract.output_address(function (error, result){
                                 if(error) console.log(error);
-                                else my_task_list[index].output_address = result;
+                                else my_task_list[taskInfo.index].output_address = result;
                             });
                             taskInfo.contract.parameters(function (error, result) {
                                 if(error) console.log(error);
-                                else {
-                                    my_task_list[index].parameters = result;
-                                }
+                                else my_task_list[taskInfo.index].parameters = result;
+
                             });
-                            my_task_list[index].address = result[i];                
+                            my_task_list[taskInfo.index].address = result[i];
+
+                            index++;
                         }
+                        //loadHistoryList();  
                     }
                 });
             }
@@ -111,7 +117,7 @@ const get_task_history = function (from, to ) {
             loadHistoryList();
         },1000);
     });
-};
+})();
 
 const loadTask = function (index) {
     let t = my_task_list[index];
@@ -131,8 +137,9 @@ const loadTask = function (index) {
 
 };
 
-const loadHistoryList = function (){                       
+const loadHistoryList = function (){
     console.log(my_task_list);
+    
     $.each(my_task_list, function(index, value){   
         $('#history-loading').hide();                    
         $("ul.histList").append("<li><a href='javascript:loadTask(" + index + ")'>" + value.task_id + " - " + value.name + "</a></li>");
@@ -141,5 +148,22 @@ const loadHistoryList = function (){
     loadTask(0);
 };
 
-get_task_history();
 
+
+//get_task_history();
+
+const showTaskResult = function(){
+
+    let uuid = $("#uuid").html();
+    let testString = $("#input").val();
+    data = JSON.stringify({"uuid":uuid,"test_string":testString});
+    $.ajax({
+        url:"http://ec2-18-220-218-90.us-east-2.compute.amazonaws.com/evaluation",
+        method:"POST",
+        data:{data:data},
+        success:function (res) {
+            $("#result").html(res)
+        }
+    })
+
+};
